598daf47809b60491bd33eaeb0413763dd21394a
johanngrabner-lv <johann.grabner@lv.campus02.at>
2020-10-08 09:45:19 +0200

    Mathe-Aufgabe

--- VBGrundlagen/VBOne/ModulMatheUebung.vb
+++ VBGrundlagen/VBOne/ModulMatheUebung.vb
@@ -1,8 +1,52 @@
 ﻿'Sub Begruessung()
 'Function ZufälligeZahlzwischen0Und10()
 'Function RichtigOderFalsch(richtig, falschezahl) As Boolean
-'Sub ErgebnisAnzeigen(rechnung als String, eingabe, richtige Ergebnis, richti / falsch)
+'Sub ErgebnisAnzeigen(rechnung als String, eingabe, richtige Ergebnis, richtig / falsch)
+' 7 + 3 --- 2 + 4
 
 Module ModulMatheUebung
+	Sub main()
 
+		Randomize()
+
+		Dim eingabe As Integer
+		Do
+
+
+			Dim z1 As Integer, z2 As Integer, ergebnis As Integer
+			Begruessung()
+
+			z1 = GetRandomNumber(10)
+			z2 = GetRandomNumber(10)
+			ergebnis = z1 + z2
+
+			Console.WriteLine("Aufgabe {0} + {1} = {2}", z1, z2, "?")
+
+			eingabe = Console.ReadLine()
+
+			Dim isEingabeKorrekt = RichtigOderFalsch(ergebnis, eingabe)
+
+			Ausgabe(z1 & " + " & z2, ergebnis, eingabe, isEingabeKorrekt)
+		Loop While eingabe <> -1
+		'Loop Until eingabe = -1
+
+
+	End Sub
+
+	Sub Ausgabe(rechnenaufgabe As String, richtig As Integer, eingabe As Integer, richtigOderFalsch As Boolean)
+		Console.WriteLine("Die Aufgabe war {0} " &
+						  " Ihre Eingabe {1} Richtig wäre: {2} Ergebnis {3} ",
+						  rechnenaufgabe, eingabe, richtig, richtigOderFalsch)
+
+	End Sub
+	Sub Begruessung()
+		Console.WriteLine("Willkommen zum Mathe-Rätsel")
+	End Sub
+	Function GetRandomNumber(ByVal upper As Int16) As Int16
+		Return CInt(Rnd() * upper) + 1
+	End Function
+
+	Function RichtigOderFalsch(richtig As Integer, eingabe As Integer) As Boolean
+		Return richtig = eingabe
+	End Function
 End Module
---
+++ VBGrundlagen/VBOne/ModuleWiederholungDonnerstag.vb
@@ -0,0 +1,76 @@
+﻿
+Module ModuleWiederholungDonnerstag
+	'Access Modifier - Zugriff
+	Public Oeffentlich As String
+	Private PrivateVariable As String
+	Public Const Pi As Decimal = 3.14 'Literal --> Fix
+
+	Sub Main()
+
+		Dim i As Integer 'Local Scope
+		Dim i2 As Int32
+
+		Dim s As Short
+		Dim s1 As Int16 'CTS -- Common Type System
+
+		If i > 10 Then
+			Dim l As Long
+			l = 20
+		End If
+		Demo(i)
+
+	End Sub
+
+	Sub Demo(eineZahl As Integer)
+		Select Case eineZahl
+			Case 1
+			Case 2
+			Case 3
+			Case Else
+
+		End Select
+
+		If eineZahl < 10 Then
+		ElseIf eineZahl < 100 Then
+		Else
+
+		End If
+
+		'nachprüfend
+		Do
+
+		Loop While eineZahl < 100
+
+		'Vorprüfende Schleife
+		While eineZahl < 100
+
+		End While
+
+
+		For i As Int16 = -10 To 10 Step +2
+
+		Next i
+
+		Dim zz As Int16
+
+		zz = GetRandomNumber(100)
+		zz = GetRandomNumber()
+		zz = GetRandomNumber(5, 200)
+	End Sub
+
+	Function GetRandomNumber() As Int16
+		Return GetRandomNumber(9)
+	End Function
+
+	'overload - überladung
+	Function GetRandomNumber(upper As Int16) As Int16
+		Return CInt(Rnd() * upper) + 1
+	End Function
+
+	Function GetRandomNumber(lower As Int16, upper As Int16) As Int16
+		Return GetRandomNumber(upper) + lower
+	End Function
+
+
+
+End Module
